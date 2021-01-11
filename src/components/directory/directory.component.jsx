b44db5239deee97bfd7a6523a4e15449366c9c58
Tabby Garcia <tabby.garcia1@gmail.com>
2021-01-11 11:33:02 -0600

    added google font for main body

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,32 +11,32 @@ class Directory extends React.Component {
         this.state = {
             sections: [
                 {
-                    title: 'hats',
+                    title: 'HATS',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
                     linkUrl: 'shop/hats'
                 },
                 {
-                    title: 'jackets',
+                    title: 'JACKETS',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
                     linkUrl: 'shop/jackets'
                 },
                 {
-                    title: 'sneakers',
+                    title: 'SNEAKERS',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     id: 3,
                     linkUrl: 'shop/sneakers'
                 },
                 {
-                    title: 'womens',
+                    title: 'WOMENS',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                 },
                 {
-                    title: 'mens',
+                    title: 'MENS',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
