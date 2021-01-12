a5f1f49be1fc3c0bd7267e434aba9ba729541a08
Tabby Garcia <tabby.garcia1@gmail.com>
2021-01-11 18:45:53 -0600

    fixed styling for content

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,32 +11,32 @@ class Directory extends React.Component {
         this.state = {
             sections: [
                 {
-                    title: 'HATS',
+                    title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
                     linkUrl: 'shop/hats'
                 },
                 {
-                    title: 'JACKETS',
+                    title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
                     linkUrl: 'shop/jackets'
                 },
                 {
-                    title: 'SNEAKERS',
+                    title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     id: 3,
                     linkUrl: 'shop/sneakers'
                 },
                 {
-                    title: 'WOMENS',
+                    title: 'womens',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                 },
                 {
-                    title: 'MENS',
+                    title: 'mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
