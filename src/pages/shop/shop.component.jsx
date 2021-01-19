fb69cdb2cf2fc5ee07da351641d38a6e9882f805
Tabby Garcia <tabby.garcia1@gmail.com>
2021-01-18 18:26:22 -0600

    getting shop page set up. I have a variable that takes in the title and the items in the shop page. Now I need to only display 4.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 
 class ShopPage extends React.Component {
@@ -13,8 +14,17 @@ class ShopPage extends React.Component {
 
 
     render() {
+        const {collections} = this.state;
         return (
-            <div>SHOP PAGE</div>
+            <div className='shop-page'>
+                {
+                    collections.map(({id, ...otherCollectionProps })=> (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+
+
+            </div>
         )
     }
 }
