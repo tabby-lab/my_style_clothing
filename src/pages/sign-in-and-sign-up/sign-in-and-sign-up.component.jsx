e478ee418ba09f6b7785d22675e2a000d7de9dbf
Tabby Garcia <tabby.garcia1@gmail.com>
2021-02-03 17:44:30 -0600

    set up sign in and sign up page, very basic. brought in the sign in page to app.js

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndUpPage = () => (
+<div className='sign-in-and-up'>SIGN IN</div>
+
+
+);
+
+export default SignInAndUpPage;
