6fbcb290b7f67da227c7651f4fadca2f80ef8137
Tabby Garcia <tabby.garcia1@gmail.com>
2021-02-04 16:02:56 -0600

    made the form for sign in with email and password. handle submit and handle change for the feild.brought it into pages for sign in.now need to style

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,27 +1,58 @@
 import React from 'react';
 
-import './sign-in.style.scss';
+import './sign-in.styles.scss';
 
 //use class bc i have to store what the user is typing in
 
-class SignIn extends React.Component{
-    constructor(props){
+class SignIn extends React.Component {
+    constructor(props) {
         super(props);
 
         this.state = {
-            email:'',
-            password:''
+            email: '',
+            password: ''
         }
 
     }
 
-    render(){
-        return(
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({ email: '', password: '' })
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({[name]: value })
+    }
+
+
+
+    render() {
+        return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        name='email'
+                        type='email'
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required />
+                    <label>Email</label>
+                    <input
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        required />
+                    <label>Password</label>
+
+                    <input type='submit' value='Submit Form' />
+
 
                 </form>
             </div>
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import SignIn from '../../components/sign-in/sign-in.component'
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndUpPage = () => (
-<div className='sign-in-and-up'>SIGN IN</div>
+<div className='sign-in-and-up'>
+    <SignIn/>
+    </div>
 
 
 );
