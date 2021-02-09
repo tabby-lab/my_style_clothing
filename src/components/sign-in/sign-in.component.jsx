aab8c598b7c3bde71728bbfb9850260bbaa1ded4
Tabby Garcia <tabby.garcia1@gmail.com>
2021-02-09 12:51:48 -0600

    created a form input component to bring into sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
 
 //use class bc i have to store what the user is typing in
@@ -36,20 +37,22 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input
+                    <FormInput
                         name='email'
                         type='email'
                         value={this.state.email}
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label='email'
                         required />
-                    <label>Email</label>
-                    <input
+                    
+                    <FormInput
                         name='password'
                         type='password'
                         value={this.state.password}
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label='password'
                         required />
-                    <label>Password</label>
+                   
 
                     <input type='submit' value='Submit Form' />
 
