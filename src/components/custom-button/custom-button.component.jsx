39a18fd5cb7d21ded983cb90cbfb880386a7ed71
Tabby Garcia <tabby.garcia1@gmail.com>
2021-02-11 18:03:28 -0600

    created very basic custom button component

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './custom-button.styles.scss';
+
+const CustomButton = () => (
+    <button className='custom-buttom'>
+
+    </button>
+);
+
+export default CustomButton;
