378ca1f96f30a452e9a8699d8735f31910b4b8d9
Tabby Garcia <tabby.garcia1@gmail.com>
2021-02-25 14:30:08 -0600

    styling complete on custom button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 //use class bc i have to store what the user is typing in
@@ -54,7 +55,7 @@ class SignIn extends React.Component {
                         required />
                    
 
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit'>Sign in</CustomButton> 
 
 
                 </form>
